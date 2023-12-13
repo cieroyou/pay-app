fe2ab5b0ceccaa4529c7718785b92f0f04eb675a
sera <cieroyou1@gmail.com>
2023-12-13 23:05:57 +0900

    스웨거 적용

---
+++ membership-service/src/main/java/com/sera/payapp/membership/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.sera.payapp.membership;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// components.securitySchemes.BearerAuth.type: http
+//components.securitySchemes.BearerAuth.scheme: bearer
+//@io.swagger.v3.oas.annotations.security.SecurityScheme(
+//        name = "bearerAuth",
+//        type = SecuritySchemeType.HTTP,
+//        bearerFormat = "JWT",
+//        scheme = "bearer"
+//)
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI getOpenApi() {
+        Info info = new Info();
+
+        return new OpenAPI()
+                .info(info);
+    }
+
+}
