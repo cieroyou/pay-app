7587dc29dc3f5589b153de89ab790c0a62d47d86
sera <cieroyou1@gmail.com>
2024-01-07 23:47:41 +0900

    feat: 내부 서비스 통신용 CommonHttpClient 구현부 작성, Bank Account 등록 하는 비즈니스 로직에 membership 유효하지 않은 경우 null 반환하도록 로직 수정, membership 서비스에 membership 유효한지 rest api GET membership 호출하여 확인하는 로직 구현

--- banking-service/src/main/java/com/sera/payapp/banking/application/service/RegisterBankAccountService.java
+++ banking-service/src/main/java/com/sera/payapp/banking/application/service/RegisterBankAccountService.java
@@ -5,8 +5,10 @@ import com.sera.payapp.banking.adapter.out.persistence.registered_bank_account.R
 import com.sera.payapp.banking.adapter.out.persistence.registered_bank_account.RegisteredBankAccountMapper;
 import com.sera.payapp.banking.application.port.in.RegisterBankAccountCommand;
 import com.sera.payapp.banking.application.port.in.RegisterBankAccountUseCase;
+import com.sera.payapp.banking.application.port.out.GetMembershipPort;
 import com.sera.payapp.banking.application.port.out.RegisterBankAccountPort;
 import com.sera.payapp.banking.application.port.out.RequestBankAccountInfoPort;
+import com.sera.payapp.banking.application.port.out.dto.MembershipStatus;
 import com.sera.payapp.banking.domain.RegisteredBankAccount;
 import com.sera.payapp.common.UseCase;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +21,18 @@ public class RegisterBankAccountService implements RegisterBankAccountUseCase {
 
     private final RegisterBankAccountPort registerMemberAccountPort;
     private final RequestBankAccountInfoPort requestBankAccountInfoPort;
+    private final GetMembershipPort getMembershipPort;
     private final RegisteredBankAccountMapper registeredBankAccountMapper;
 
 
     @Override
     public RegisteredBankAccount registerBankAccount(RegisterBankAccountCommand command) {
-        // TODO: 멤버쉽Id 가 정상인지 확인(to Membership Service)
+        MembershipStatus membershipStatus = getMembershipPort.getMembershipStatus(command.getMembershipId());
+        if (!membershipStatus.isValid()) {
+            // TODO: 에러 정의 필요, null 대신 에러 Response 보내도록 처리
+            return null;
+        }
+
         // 1. 외부 은행에 계좌등록 요청온 계좌가 정상인지 확인
         BankAccountInfo bankAccountInfo = requestBankAccountInfoPort.getBankAccountInfo(command.getBankName(), command.getBankAccountNumber());
         boolean isAccountValid = bankAccountInfo.isValid();
