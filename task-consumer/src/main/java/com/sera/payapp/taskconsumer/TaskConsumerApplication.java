c9469f9e9674638dde6cf1663cf2599eba8e9f0b
sera <cieroyou1@gmail.com>
2024-01-22 22:49:47 +0900

    chore: task-consumer 어플리케이션 모듈 생성

---
+++ task-consumer/src/main/java/com/sera/payapp/taskconsumer/TaskConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sera.payapp.taskconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskConsumerApplication.class, args);
+    }
+
+}
