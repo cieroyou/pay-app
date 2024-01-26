92eae22c3e7727b58a261d83fd7800e7c21c8007
sera <cieroyou1@gmail.com>
2024-01-26 11:05:23 +0900

    chore: application.yml 파일 수정, service.membership.url, service.banking.url, kafka.clusters.bootstrapservers, logging.topic 변수 세팅, common 모듈의 컴포넌트 스캔하는 @ComponentScan 어노테이션 추가한 RemittanceConfig 추가
    * why: Common의 HttpClient 를 빈으로 읽어오기 위해 @ComponentScan 세팅, common에서 사용되는 환경변수들을 필수로 넣어주기 위해 환경변수 추가

---
+++ remittance-service/src/main/java/com/sera/payapp/remittance/RemittanceConfig.java
@@ -0,0 +1,9 @@
+package com.sera.payapp.remittance;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.sera.payapp.common")
+public class RemittanceConfig {
+}
