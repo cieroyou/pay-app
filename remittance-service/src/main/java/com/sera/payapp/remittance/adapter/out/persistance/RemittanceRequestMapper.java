0afd66bb6ff17a004580952deb8fe450f996cba4
sera <cieroyou1@gmail.com>
2024-01-26 13:02:18 +0900

    feat: 송금 서비스 data access Entity, 도메인 Entity 작성(wip)

---
+++ remittance-service/src/main/java/com/sera/payapp/remittance/adapter/out/persistance/RemittanceRequestMapper.java
@@ -0,0 +1,19 @@
+package com.sera.payapp.remittance.adapter.out.persistance;
+
+import com.sera.payapp.remittance.domain.RemittanceRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RemittanceRequestMapper {
+    public RemittanceRequest mapToDomainEntity(RemittanceRequestJpaEntity remittanceRequestJpaEntity) {
+        return RemittanceRequest.generateRemittanceRequest(
+                new RemittanceRequest.RemittanceRequestId(remittanceRequestJpaEntity.getFromMembershipId()),
+                new RemittanceRequest.RemittanceFromMembershipId(remittanceRequestJpaEntity.getFromMembershipId()),
+                new RemittanceRequest.ToBankName(remittanceRequestJpaEntity.getToBankName()),
+                new RemittanceRequest.ToBankAccountNumber(remittanceRequestJpaEntity.getToBankAccountNumber()),
+                new RemittanceRequest.RemittanceType(remittanceRequestJpaEntity.getRemittanceType()),
+                new RemittanceRequest.Amount(remittanceRequestJpaEntity.getAmount()),
+                new RemittanceRequest.RemittanceStatus(remittanceRequestJpaEntity.getRemittanceStatus())
+        );
+    }
+}
