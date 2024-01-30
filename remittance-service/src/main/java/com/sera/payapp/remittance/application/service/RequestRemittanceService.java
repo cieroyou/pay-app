6ba6ad368d5934382a6860c86b8925bf9f815339
sera <cieroyou1@gmail.com>
2024-01-30 17:12:16 +0900

    feat: 송금 서비스에서 송금 요청 비즈니스 서비스 로직 구현(wip)

--- remittance-service/src/main/java/com/sera/payapp/remittance/application/service/RequestRemittanceService.java
+++ remittance-service/src/main/java/com/sera/payapp/remittance/application/service/RequestRemittanceService.java
@@ -1,11 +1,13 @@
 package com.sera.payapp.remittance.application.service;
 
 import com.sera.payapp.common.UseCase;
+import com.sera.payapp.remittance.adapter.out.persistance.RemittanceRequestJpaEntity;
 import com.sera.payapp.remittance.application.port.in.RequestRemittanceUseCase;
 import com.sera.payapp.remittance.application.port.in.dto.RequestRemittanceCommand;
 import com.sera.payapp.remittance.application.port.in.dto.RequestRemittanceInfo;
 import com.sera.payapp.remittance.application.port.out.MembershipPort;
 import com.sera.payapp.remittance.application.port.out.MoneyPort;
+import com.sera.payapp.remittance.application.port.out.RequestRemittancePort;
 import com.sera.payapp.remittance.application.port.out.dto.MembershipStatus;
 import com.sera.payapp.remittance.application.port.out.dto.MoneyInfo;
 import lombok.RequiredArgsConstructor;
@@ -18,14 +20,31 @@ public class RequestRemittanceService implements RequestRemittanceUseCase {
     private final MembershipPort membershipPort;
 
     private final MoneyPort moneyPort;
+    private final RequestRemittancePort requestRemittancePort;
 
     @Override
     public RequestRemittanceInfo requestRemittance(RequestRemittanceCommand command) {
         // 0. 송금 요청 상태를 시작 상태로 기록
+        RemittanceRequestJpaEntity entity = requestRemittancePort.createRemittanceRequestHistory(command);
+
         // 1. 고객 정보가 정상인지 확인(to membership-service)
         MembershipStatus membershipStatus = membershipPort.getMembershipStatus(command.getFromMembershipId());
+        if (!membershipStatus.isValid()) {
+            // TODO: null 대신 에러 상태를 반환하도록 함
+            return null;
+        }
         // 2. 잔액 확인(to money-service)
         MoneyInfo moneyInfo = moneyPort.getMoneyInfo(command.getFromMembershipId());
+        if (moneyInfo.getBalance() < command.getAmount()) {
+            // TODO: 잔액이 충분치 않은 경우, 만원 단위로 충전하기
+            int rechargeAmount = (int) Math.ceil((command.getAmount() - moneyInfo.getBalance()) / 10000.0) * 10000;
+            // 2-1. 잔액이 충분하지 않다면, 충전 요청 (money svc)
+            boolean moneyResult = moneyPort.requestMoneyRecharging(command.getFromMembershipId(), rechargeAmount);
+            if (!moneyResult) {
+                return null;
+            }
+
+        }
         // 2-1. 잔액이 충분하지 않은 경우, 충전 요청(to money-service)
         // 3. 송급타입 확인(고객/은행)
         // 3-1. 고객인 경우, from 고객 머니 감액, to 고객머니 증액(to banking-service)
