92d49b99c27ffdc83344e8fda4cf46fde8084376
sera <cieroyou1@gmail.com>
2024-02-20 23:03:12 +0900

    rename: PaymentApplication 파일 위치 변경

--- payment-service/src/main/java/com/sera/payapp/payment/PaymentApplication.java
+++ payment-service/src/main/java/com/sera/payapp/payment/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.sera.payapp;
+package com.sera.payapp.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
